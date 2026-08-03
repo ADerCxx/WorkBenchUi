386eebedbf99360605c38716d7c059c4b9628e2c
Cx <1226298223@qq.com>
2026-08-03 15:44:38 +0800

    fix: 主题色调整

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { ConfigProvider } from 'antd'
-import zhCN from 'antd/locale/zh_CN'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import './styles/index.global.less'
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import './styles/index.global.less';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,11 +12,11 @@ createRoot(document.getElementById('root')!).render(
       locale={zhCN}
       theme={{
         token: {
-          colorPrimary: '#aa3bff',
+          colorPrimary: '#1677ff',
         },
       }}
     >
       <RouterProvider router={router} />
     </ConfigProvider>
   </StrictMode>,
-)
+);
